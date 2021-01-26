18898cc3c9a0a2a4a785d9c3a40d116749066043
major-winter <tuanhoangchu@outlook.com>
2021-01-27 00:12:09 +0700

    update UI

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const hamburger = document.querySelector('.navbar-icon')
+const mobileIcon = document.querySelector('.navbar-icon')
+const hamburger = document.querySelector('.navbar-icon .hamburger')
+const times = document.querySelector('.navbar-icon .times')
 const navbarItems = document.querySelector('.navbar-items')
+const btn = document.querySelector('.btn')
 
-hamburger.addEventListener('click', () => {
-    navbarItems.classList.toggle('show')
-    hamburger.classList.toggle('navbar-icon-move')
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
+})
+
+mobileIcon.addEventListener('click', () => {
+    navbarItems.classList.toggle('navbar-show')
+    mobileIcon.classList.toggle('navbar-icon-move')
+    hamburger.classList.toggle('hide')
+    times.classList.toggle('hide')
 })
