95c80788e08e4d9de3d79a55b7215267feb0d61d
major-winter <tuanhoangchu@outlook.com>
2021-01-28 18:57:47 +0700

    fix main navbar upon scrolling

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const times = document.querySelector('.navbar-icon .times')
 const navbarItems = document.querySelector('.navbar-items')
 const btn = document.querySelector('.btn')
 
+const navBar = document.querySelector('.nav')
+
 btn.addEventListener('click', (event) => {
     event.preventDefault()
 })
@@ -13,4 +15,20 @@ mobileIcon.addEventListener('click', () => {
     mobileIcon.classList.toggle('navbar-icon-move')
     hamburger.classList.toggle('hide')
     times.classList.toggle('hide')
+})
+
+let scrollTop = 0
+
+document.addEventListener('scroll', () => {
+
+    if (window.scrollY > navBar.offsetHeight) {
+        navBar.classList.add('p-fixed')
+        navBar.scroll({
+            behavior: "smooth"
+        })
+    }
+
+    if (window.scrollY < navBar.offsetHeight) {
+        navBar.classList.remove('p-fixed')
+    }
 })
